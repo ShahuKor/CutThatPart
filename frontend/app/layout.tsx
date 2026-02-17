7a444a1d57cd10e02405959065a46e7e731d4ef8
ShahuKor <korshahu17@gmail.com>
2026-02-17 21:35:18 +0000

    Add Initial frontend setup and hero section

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Instrument_Sans, Instrument_Serif } from "next/font/google";
+import "./globals.css";
+
+const instrumentSans = Instrument_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const instrumentSerif = Instrument_Serif({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "CutThatPart",
+  description:
+    "A web app that allows you to Trim any YouTube video and send a clean, temporary clip link.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${instrumentSans.variable} ${instrumentSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/components/Container.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("mx-auto w-full max-w-7xl", className)}>{children}</div>
+  );
+};
